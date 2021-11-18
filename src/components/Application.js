68b07cbd5560f2fd9e19e0692165868e93107ae4
ValEmpire <palma_arcival@yahoo.com>
2021-11-18 20:06:18 +0000

    create appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./Application.scss";
+import Appointment from "./Appointment";
 import DayList from "./DayList";
 
 export default function Application(props) {
@@ -24,6 +25,45 @@ export default function Application(props) {
     },
   ];
 
+  const appointments = {
+    1: {
+      id: 1,
+      time: "12pm",
+    },
+    2: {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        },
+      },
+    },
+    3: {
+      id: 3,
+      time: "2pm",
+    },
+    4: {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer: {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        },
+      },
+    },
+    5: {
+      id: 5,
+      time: "4pm",
+    },
+  };
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -43,7 +83,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment, i) => (
+          <Appointment key={`${i}${appointment.id}`} {...appointment} />
+        ))}
       </section>
     </main>
   );
